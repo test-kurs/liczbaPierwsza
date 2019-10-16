c2f2d53fd50d585caa11db3a05f69ecf3271bfce
Jakub D <mrbestkubek@gmail.com>
2019-10-16 17:31:48 +0200

    dodanie jara oraz wydzielenie klasy MainApp

--- src/test/java/SilniaTest.java
+++ src/test/java/SilniaTest.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class SilniaTest {
+
+    @Test
+    public void czySilniaZ5To120(){
+        Silnia silnia = new Silnia();
+        assertEquals(silnia.silnia(5), 120);
+        assertEquals(silnia.silniaRekurencja(5), 120);
+    }
 }
