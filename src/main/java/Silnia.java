1a666ac8fe9b7c2836874b0b5a88eb26becba462
Jakub D <mrbestkubek@gmail.com>
2019-10-16 17:01:56 +0200

    dodanie klasy silnia

--- src/main/java/Silnia.java
+++ src/main/java/Silnia.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Silnia {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę:");
+        int liczba = scanner.nextInt();
+
+        int silniaPętla = silnia(liczba);
+        System.out.println("Silnia pętla z " + liczba + " to: " + silniaPętla);
+
+
+        int silniaRekurencja = silniaRekurencja(liczba);
+        System.out.println("Silnia rekurencyjna z "+ liczba+ " to: "+ silniaRekurencja);
+    }
+
+    public static int silnia(int liczba){
+        int silnia = 1;
+        for (int i = 1; i <= liczba; i++) {
+            silnia = silnia * i;
+        }
+        return silnia;
+    }
+
+    public static int silniaRekurencja(int n){ return (n == 0) ? 1 : (n * silnia(n-1)); }
 }
